55c411e6d500c26b03be7cae6712430f1dd259da
Prabh231 <200651351@student.georgianc.on.ca>
2026-06-03 23:25:39 -0400

    some changes

--- index.php
+++ index.php
@@ -1,16 +1,11 @@
 <?php
 
 require_once "config.php";
-require_once "LessonWeatherHandler.php";
+require_once "weatherHandler.php";
 
-$lessonActiveCity = isset($_GET['city'])
-    ? trim($_GET['city'])
-    : "Toronto";
+$lessonActiveCity = isset($_GET['city']) ? trim($_GET['city'])   : "Toronto";
 
-$lessonHandler = new LessonWeatherHandler(
-    WEATHER_BASE_URL,
-    WEATHER_API_KEY
-);
+$lessonHandler = new weatherHandler(WEATHER_BASE_URL,WEATHER_API_KEY);
 
 $lessonWeatherData = $lessonHandler->fetchWeather($lessonActiveCity);
 
--- views/weather.view.php
+++ views/weather.view.php
@@ -1,17 +1,16 @@
 <!doctype html>
 <html lang="en">
 <head>
-    ...
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
 <main class="container">
 
     <section class="row">
-        <h2>
-            Weather Report -
-            <?php echo htmlspecialchars($lessonActiveCity); ?>
-        </h2>
+        <h2>Weather Report -<?php echo htmlspecialchars($lessonActiveCity); ?></h2>
     </section>
 
     <section class="row">
